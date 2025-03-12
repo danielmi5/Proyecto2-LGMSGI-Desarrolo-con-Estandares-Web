58ba74dd0ddf306b11ab49936c6a4981cd05d234
danielmi5 <midaniel@iesrafaelalberti.es>
2025-03-12 11:42:37 +0100

    Apartado 3 de manipulación del DOM realizado

--- js/script.js
+++ js/script.js
@@ -5,6 +5,14 @@
 const elementoId = document.getElementById("imagenWeb");
 console.log(elementoId);
 
+//querySelector() seleccionar el primer elemento dentro del DOM que coincide con un selector CSS
+
+const primerSection = document.querySelector("section") //Selecciona el primer section en el documento
+console.log(primerSection)
+
+//querySelectorAll() selecciona todos los elementos que coinciden con un selector CSS especificado
+const todosSection = document.querySelectorAll("section") //Selecciona todos los elemenots section del documento
+console.log(todosSection)
 //Siguientes métodos devuelven un htmlcollection, si no coinciden elementos, devuelven un array vacío [].
 
 //getElementByClassName() devuelve una colección con los elementos que coincidan con esa clase 'cabecera'.
@@ -18,7 +26,7 @@ console.log(elementoName);
 //getElementByTagName() devuelve un colección con los elementos que coincidan con esa etiqueta 'a' (enlaces).
 const elementoTag = document.getElementsByTagName("a");
 console.log(elementoTag); 
-*/
+
 
 
 
@@ -64,7 +72,65 @@ for (let pos = 0; pos < redes.length; pos++) {
 
 //Modificar el contenido y atributos de elementos existentes.
 
+/* CONTENIDO
 
+/*
+//textContent obtiene el contenido (texto) de un elemento
+let titulo = document.querySelector(".intro__titulo");
+console.log(titulo.textContent); //muestra el contenido del texto antes de modificar
+titulo.textContent = "Probando cambios de contenido"; //modifica el contenido
+console.log(titulo.textContent); // muestra el contenido cambiado
+
+//innerText obtiene el contenido HTML de un elemento (incluido las etiquetas). Es decir que si de modifica un elemento con innerHTML eliminaría todo el contenido HTML.
+const seccionIntro = document.querySelector(".intro");
+console.log(seccionIntro.innerHTML) // Muestra el contenido HTML de la sección intro
+seccionIntro.innerHTML = "<img src='../assets/ser3.jpg' width='1000px' height='853px'>Contenido de la sección cambiado por esta img" //Esta etiqueta está incompleto, pero con innerHTML el navegador comprueba y lo parsea, intentando que el código HTML sea correcto en todo momento
+console.log(seccionIntro.innerHTML);
+
+//outerText obtiene el contenido HTML de un elemento (incluyendo el html de dicho elemento)
+let listaRedes = document.querySelector(".pie__redes .pie__lista");
+console.log(listaRedes); //Muestra todo el html de listaRedes (ul), incluyendo su etiqueta y su contenido HTML
+listaRedes.outerHTML = "<p>He pasado a ser un párrafo</p>" //Ahora en vez de ser una ul con todo su contenido, es una etiqueta <p>
+
+//.setHTMLUnsafe() es una alternativa moderna para la propiedad .innerHTML (solo para modificar). Tiene la peculiaridad de que cuenta con soporte de DOM Shadow declarativo. Para obtenerlo sería con .getHTML() (igualmente compatible con DOM Shadow declarativo)
+//Esto es otra manera de hacer HTML con ``
+let html = `
+  <ul>
+    <li>
+      <p>Hola</p>
+    </li>
+    <li>
+      <p>Interesante, pero no muy seguro</p>
+    </li>
+    <li>
+      <p>3 linea</p>
+    </li>
+  </ul>
+`;
+const pieContacto = document.querySelector(".pie__lista"); //Cojo la primera lista del footer
+pieContacto.setHTMLUnsafe(html); //Y le camvia el contenido por el HTML de html
+*/
+
+//ATRIBUTOS
+
+/*
+//Para obtener atributos
+const elementoPrueba = document.querySelector(".cabecera__logo")
+//hasAttributes()	Indica si el elemento tiene atributos HTML.
+console.log(elementoPrueba.hasAttributes())
+//hasAttribute(??)	Indica si el elemento tiene el atributo HTML ??.
+console.log(elementoPrueba.hasAttribute("style"))
+//getAttributeNames()	Devuelve un array con los atributos del elemento.
+console.log(elementoPrueba.getAttributeNames())
+//getAttribute(??)	Devuelve el valor del atributo ?? del elemento o null si no existe.
+console.log(elementoPrueba.getAttribute("src"))
+
+//Para modificar se coloca el nombre de la variable seguido de un punto y el nombre del atributo --> name.atributo
+elementoPrueba.src = "assets/ser3.jpg"
+elementoPrueba.title = "NO LOGO"
+//Otra manera sería con .setAttribute(nombreAtributo, valorAtributo)
+elementoPrueba.setAttribute("alt", "Imagen que no es un logo")
+*/
 
 // Eliminar elementos de la página según criterios específicos.
 
